97a1f1c6082d747733c79d4167c85c5bf170afc5
rexweb <niveditakumari@it.mitaoe.ac.in>
2021-06-01 15:50:23 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@
 
 
 import * as React from 'react';
-import ReactMapGL,{GeolocateControl} from 'react-map-gl';
+import ReactMapGL,{GeolocateControl,Marker} from 'react-map-gl';
 import "./styles.css"
-// import { Marker } from 'react-mapbox-gl';
+import DeckGL from '@deck.gl/react';
+import {LineLayer,GeoJsonLayer} from '@deck.gl/layers';
 const geolocateControlStyle= {
   right: 10,
   top: 10
 };
+
 
 const geojson = {
   type: 'FeatureCollection',
@@ -18,18 +20,10 @@ const geojson = {
   ]
 };
 
-const layerStyle = {
-  id: 'point',
-  type: 'circle',
-  paint: {
-    'circle-radius': 10,
-    'circle-color': '#007cbf'
-  }
-};
 
 
 
-function App() {
+function App({viewState,data}) {
   const [viewport, setViewport] = React.useState({
     latitude: 37.7577,
     longitude: -122.4376,
@@ -39,6 +33,62 @@ function App() {
     //  pitch:50
   });
   const [parkColor, setParkColor] = React.useState('#8fa');
+  // const layer = new LineLayer({
+  //   id: 'line-layer',
+  //   data,
+  //   pickable: true,
+  //   getWidth: 50,
+  //   getSourcePosition: d => d.from.coordinates,
+  //   getTargetPosition: d => d.to.coordinates,
+  //   getColor: d => [Math.sqrt(d.inbound + d.outbound), 140, 0]
+  // });
+
+
+
+
+  data = [{
+    inbound: 72633,
+   outbound: 74735,
+    from: {
+      name: 'chnadigard',
+     coordinates: [ 37.7577, -122.4376]
+   },
+    to: {
+      name: '12th St. Oakland City Center (12TH)',
+      coordinates: [31.0048, 75.9463]
+  }   }  
+]
+
+
+  // const layer = new LineLayer({
+  //   id: 'line-layer',
+  //   data,
+  //   pickable: true,
+  //   getWidth: 50,
+  //   getSourcePosition: d => d.from.coordinates,
+  //   getTargetPosition: d => d.to.coordinates,
+  //   getColor: d => [Math.sqrt(d.inbound + d.outbound), 140, 0]
+  // });
+
+  const layer = new GeoJsonLayer({
+    id: 'geojson-layer',
+    data,
+    pickable: true,
+    stroked: false,
+    filled: true,
+    extruded: true,
+    lineWidthScale: 20,
+    lineWidthMinPixels: 2,
+    getFillColor: [160, 160, 180, 200],
+    // getLineColor: d => colorToRGBArray(d.properties.color),
+    getRadius: 100,
+    getLineWidth: 1,
+    getElevation: 30
+  });
+
+
+
+  
   
   return (
     <ReactMapGL
@@ -49,7 +99,7 @@ function App() {
      
 
       onViewportChange={(newView) => setViewport(newView)}
-      attributionControl={false} 
+    
       > 
       <GeolocateControl
         style={geolocateControlStyle}
@@ -57,14 +107,82 @@ function App() {
         trackUserLocation={true}
         auto
       />
-      <ScaleControl maxWidth={100} unit="metric" style={scaleControlStyle} />
-      <Source id="my-data" type="geojson" data={geojson}>
-        <Layer {...layerStyle} />
-      </Source>
-      
+    
+       
+       <Marker latitude={31.0048} longitude={75.9463} offsetTop={(-viewport.zoom * 5) / 2}>
+        <img src="https://cdn0.iconfinder.com/data/icons/small-n-flat/24/678111-map-marker-512.png" width={viewport.zoom * 5} height={viewport.zoom * 5}
+        
+        />
+        
+        
+        </Marker>  
+
+{/* 
+        <DeckGL viewState={viewState}
+    layers={[layer]}
+    getTooltip={({object}) => object && `${object.from.name} to ${object.to.name}`} />; */}
+
+<DeckGL viewState={viewState}
+    layers={[layer]}
+    getTooltip={({object}) => object && (object.properties.name || object.properties.station)} />;
+
+     
       </ReactMapGL>
     );
   }
   
   export default App;
 
+
+
+
+
+// import React, { useRef, useEffect, useState } from 'react';
+// import mapboxgl from 'mapbox-gl';
+// import './App.css';
+
+// mapboxgl.accessToken =
+//   'pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4M29iazA2Z2gycXA4N2pmbDZmangifQ.-g_vE53SD2WrJ6tFX7QHmA';
+
+// const App = () => {
+//   const mapContainerRef = useRef(null);
+
+//   const [lng, setLng] = useState(5);
+//   const [lat, setLat] = useState(34);
+//   const [zoom, setZoom] = useState(1.5);
+
+//   // Initialize map when component mounts
+//   useEffect(() => {
+//     const map = new mapboxgl.Map({
+//       container: mapContainerRef.current,
+//       style: 'mapbox://styles/mapbox/streets-v11',
+//       center: [lng, lat],
+//       zoom: zoom
+//     });
+
+//     // Add navigation control (the +/- zoom buttons)
+//     map.addControl(new mapboxgl.NavigationControl(), 'top-right');
+
+//     map.on('move', () => {
+//       setLng(map.getCenter().lng.toFixed(4));
+//       setLat(map.getCenter().lat.toFixed(4));
+//       setZoom(map.getZoom().toFixed(2));
+//     });
+
+//     // Clean up on unmount
+//     return () => map.remove();
+//   }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+//   return (
+//     <div>
+//       <div className='sidebarStyle'>
+//         <div>
+//           Longitude: {lng} | Latitude: {lat} | Zoom: {zoom}
+//         </div>
+//       </div>
+//       <div className='map-container' ref={mapContainerRef} />
+//     </div>
+//   );
+// };
+
+// export default App;
